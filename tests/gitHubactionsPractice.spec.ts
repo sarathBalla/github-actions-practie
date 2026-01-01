886b6a84487dc04563e3c4901ac6847d0ec2217f
Sarath Balla <sarathballa143@gmail.com>
2026-01-01 14:05:58 +0530

    minor changes

---
+++ tests/gitHubactionsPractice.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test';
+
+test('Practice Test 1', async ({ page }) => {
+    await page.goto('https://www.saucede.com');
+    const title = await page.title();
+    expect(page).toHaveTitle(title);
+    console.log("Title is " + title);
+    console.log("Practice Test 1 executed successfully");
+});
+
+test('Practice Test 2', async ({ page }) => {
+    await page.goto('https://www.saucede.com');
+    const title = await page.title();
+    expect(page).toHaveTitle(title);
+    console.log("Title is " + title);
+    console.log("Practice Test 2 executed successfully");
+});
+
+test('Practice Test 3', async ({ page }) => {
+    await page.goto('https://www.saucede.com');
+    const title = await page.title();
+    expect(page).toHaveTitle(title);
+    console.log("Title is " + title);
+    console.log("Practice Test 3 executed successfully");
+});
+test('Practice Test 4', async ({ page }) => {
+    await page.goto('https://www.saucede.com');
+    const title = await page.title();
+    expect(page).toHaveTitle(title);
+    console.log("Title is " + title);
+    console.log("Practice Test 4 executed successfully");
+});
+test('Practice Test 5', async ({ page }) => {
+    await page.goto('https://www.saucede.com');
+    const title = await page.title();
+    expect(page).toHaveTitle(title);
+    console.log("Title is " + title);
+    console.log("Practice Test 5 executed successfully");
+});
+test('Practice Test 6', async ({ page }) => {
+    await page.goto('https://www.saucede.com');
+    const title = await page.title();
+    expect(page).toHaveTitle(title);
+    console.log("Title is " + title);
+    console.log("Practice Test 6 executed successfully");
+});
+test('Practice Test 7', async ({ page }) => {
+    await page.goto('https://www.saucede.com');
+    const title = await page.title();
+    expect(page).toHaveTitle(title);
+    console.log("Title is " + title);
+    console.log("Practice Test 7 executed successfully");
+});
