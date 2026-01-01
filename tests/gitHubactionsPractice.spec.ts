cdfdb2ac3c3b121a4392c3613937da19423cdb79
Sarath Balla <sarathballa143@gmail.com>
2026-01-01 14:17:22 +0530

    #Updated proper url

--- tests/gitHubactionsPractice.spec.ts
+++ tests/gitHubactionsPractice.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Practice Test 1', async ({ page }) => {
-    await page.goto('https://www.saucede.com');
+    await page.goto('https://www.saucedemo.com/');
     const title = await page.title();
     expect(page).toHaveTitle(title);
     console.log("Title is " + title);
@@ -9,7 +9,7 @@ test('Practice Test 1', async ({ page }) => {
 });
 
 test('Practice Test 2', async ({ page }) => {
-    await page.goto('https://www.saucede.com');
+    await page.goto('https://www.saucedemo.com/');
     const title = await page.title();
     expect(page).toHaveTitle(title);
     console.log("Title is " + title);
@@ -17,35 +17,35 @@ test('Practice Test 2', async ({ page }) => {
 });
 
 test('Practice Test 3', async ({ page }) => {
-    await page.goto('https://www.saucede.com');
+    await page.goto('https://www.saucedemo.com/');
     const title = await page.title();
     expect(page).toHaveTitle(title);
     console.log("Title is " + title);
     console.log("Practice Test 3 executed successfully");
 });
 test('Practice Test 4', async ({ page }) => {
-    await page.goto('https://www.saucede.com');
+    await page.goto('https://www.saucedemo.com/m');
     const title = await page.title();
     expect(page).toHaveTitle(title);
     console.log("Title is " + title);
     console.log("Practice Test 4 executed successfully");
 });
 test('Practice Test 5', async ({ page }) => {
-    await page.goto('https://www.saucede.com');
+    await page.goto('https://www.saucedemo.com/');
     const title = await page.title();
     expect(page).toHaveTitle(title);
     console.log("Title is " + title);
     console.log("Practice Test 5 executed successfully");
 });
 test('Practice Test 6', async ({ page }) => {
-    await page.goto('https://www.saucede.com');
+    await page.goto('https://www.saucedemo.com/');
     const title = await page.title();
     expect(page).toHaveTitle(title);
     console.log("Title is " + title);
     console.log("Practice Test 6 executed successfully");
 });
 test('Practice Test 7', async ({ page }) => {
-    await page.goto('https://www.saucede.com');
+    await page.goto('https://www.saucedemo.com/');
     const title = await page.title();
     expect(page).toHaveTitle(title);
     console.log("Title is " + title);
