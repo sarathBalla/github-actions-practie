179a32dc787f35c8e0239671db2a7b4a5144389b
Sarath Balla <sarathballa143@gmail.com>
2026-02-08 23:31:45 +0530

    #UpdatedBrowserDetails

--- tests/gitHubactionsPractice.spec.ts
+++ tests/gitHubactionsPractice.spec.ts
@@ -1,27 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('Practice Test 1', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    const title = await page.title();
-    expect(page).toHaveTitle(title);
-    console.log("Title is " + title);
-    console.log("Practice Test 1 executed successfully");
-});
+test.describe('GitHub Actions Practice Tests', () => {
+    test('Practice Test 1', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        const title = await page.title();
+        expect(page).toHaveTitle(title);
+        console.log("Title is " + title);
+        console.log("Practice Test 1 executed successfully");
+    });
 
-test('Practice Test 2', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    const title = await page.title();
-    expect(page).toHaveTitle(title);
-    console.log("Title is " + title);
-    console.log("Practice Test 2 executed successfully");
-});
+    test('Practice Test 2', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        const title = await page.title();
+        expect(page).toHaveTitle(title);
+        console.log("Title is " + title);
+        console.log("Practice Test 2 executed successfully");
+    });
 
-test('Practice Test 3', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    const title = await page.title();
-    expect(page).toHaveTitle(title);
-    console.log("Title is " + title);
-    console.log("Practice Test 3 executed successfully");
+    test('Practice Test 3', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        const title = await page.title();
+        expect(page).toHaveTitle(title);
+        console.log("Title is " + title);
+        console.log("Practice Test 3 executed successfully");
+    });
 });
 test('Practice Test 4', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/m');
